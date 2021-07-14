7f1fafa420ce073dc144553de909c109ce10d34e
Dorian Smalley <smalley2001@fb.com>
2021-07-14 16:35:16 -0400

    Receives user's location and adjust camera to focus on user location

--- app/src/main/java/com/codepath/tout_le_monde/GoogleMapsActivity.java
+++ app/src/main/java/com/codepath/tout_le_monde/GoogleMapsActivity.java
@@ -21,17 +21,31 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+
+import org.jetbrains.annotations.NotNull;
 
 public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -41,12 +55,51 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
     // and GoogleMap objects in more detail.
 
 
-    private GoogleMap mMap;
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
-    private Boolean mLocationPermissionsGranted = false;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
     private static final String TAG = "GoogleMapsActivity";
+
+    private GoogleMap mMap;
+    private Boolean mLocationPermissionsGranted = false;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+
+    // mLocationCallback is a public variable for getting the location
+    //This is because we want to already have a location stored so when
+    //we call for location updates later, there won't be a bug because
+    //if our original location was null, it could cause issues
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            if (locationResult == null) {
+                Log.i(TAG, "no location for update");
+                return;
+            }
+            for (Location location : locationResult.getLocations()) {
+                if (location != null) {
+                    Log.i(TAG, location.toString());
+                    //Move camera to my location
+                    moveCamera(new LatLng(location.getLatitude(), location.getLongitude()), DEFAULT_ZOOM);
+
+                    //Must explicitly ask for permission in order to setMyLocationEnabled
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    //This sets a blue dot on the map to indicate where you are located
+                    mMap.setMyLocationEnabled(true);
+                    //Stops app from constantly asking for locations
+                    //This because if the app keeps requesting updates then it will
+                    //significantly drain the user's battery, harming the UX
+                    LocationServices.getFusedLocationProviderClient(getApplicationContext()).removeLocationUpdates(mLocationCallback);
+                    //TODO: UI updates.
+                }
+            }
+        }
+    };
 
 
     @Override
@@ -58,9 +111,9 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         //The SupportMapFragment object manages the life cycle of the map and is the
         // parent element of the app's UI.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
     }
 
     /**
@@ -68,7 +121,7 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
      * we just add a marker near Sydney, Australia.
-     *
+     * <p>
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -79,18 +132,40 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
         Log.d(TAG, "onMapReady: map is ready");
         mMap = googleMap;
 
+        //When Map is ready, onMapReady function is called
+        //If we have location permissions then we can request the
+        //location
+        if (mLocationPermissionsGranted) {
+            LocationRequest mLocationRequest = LocationRequest.create();
+            //Longest our request will go is 1 minute
+            mLocationRequest.setInterval(60000);
+            //Shortest our request will go is 5 seconds
+            mLocationRequest.setFastestInterval(5000);
+            //We want the location with high accuracy
+            mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+            //Even though we already have location permissions
+            //Android Studi requires that we explicitly ask for permissions
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                //Request location only once (no looper needed)
+                LocationServices.getFusedLocationProviderClient(this).requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+            }
+        }
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions()
+//                .position(sydney)
+//                .title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     private void getLocationPermission() {
         Log.d(TAG, "getting location permissions");
         String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.ACCESS_COARSE_LOCATION};
+                Manifest.permission.ACCESS_COARSE_LOCATION};
 
         //Check if we have permission to access the user's location
 
@@ -101,6 +176,7 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
                     COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
                 mLocationPermissionsGranted = true;
+                initMap();
             }
 
             // If we don't have permission for COARSE Location then we must ask for it!
@@ -118,19 +194,29 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
 
     }
 
+    public void initMap() {
+
+
+        Log.d(TAG, "initMap : initializing map");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(GoogleMapsActivity.this);
+    }
+
+    //This function is called after Permissions have been requested
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull @org.jetbrains.annotations.NotNull String[] permissions, @NonNull @org.jetbrains.annotations.NotNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         Log.d(TAG, "onRequestPermissionsResult : called");
-        mLocationPermissionsGranted= false;
+        mLocationPermissionsGranted = false;
 
         switch (requestCode) {
             case LOCATION_PERMISSION_REQUEST_CODE: {
-                if(grantResults.length > 0) {
+                if (grantResults.length > 0) {
 
-                    for(int i = 0; i < grantResults.length; i++) {
-                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
                             mLocationPermissionsGranted = false;
                             Log.d(TAG, "onRequestPermissionsResult: permission failed");
                             return;
@@ -138,10 +224,55 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
                     }
                     Log.d(TAG, "Permissions granted");
                     mLocationPermissionsGranted = true;
+                    initMap();
 
                 }
             }
         }
+    }
+
+    //Location can return null,
+
+    private void getDeviceLocation() {
+        Log.d(TAG, "getDeviceLocation: getting the current devices location");
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try {
+            if (mLocationPermissionsGranted) {
+                Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "onComplete: found location!");
+                            Location currentLocation = (Location) task.getResult();
+
+                            if (currentLocation == null) {
+                                Log.d(TAG, "There is not a location!");
+                            } else {
+                                Log.d(TAG, "The latitude is: " + currentLocation.getLatitude());
+
+                                moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
+
+                            }
+
+
+                        } else {
+                            Log.d(TAG, "onComplete: current location is null");
+                            Toast.makeText(GoogleMapsActivity.this, "unable to get current location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+
+        } catch (SecurityException e) {
+            Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage());
+        }
+    }
+
+    private void moveCamera(LatLng latlng, float zoom) {
+        Log.d(TAG, "moveCamera: moving the camera to: lat: " + latlng.latitude + ", lng: " + latlng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, zoom));
     }
 }
 
