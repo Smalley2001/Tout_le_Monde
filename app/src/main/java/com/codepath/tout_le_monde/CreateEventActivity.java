f06df8def10771d45a58dd92aa6add818b149fcf
Dorian Smalley <smalley2001@fb.com>
2021-07-16 15:53:39 -0400

    Updated Event object to represent the fields in the Parse database. Updated Create Event Page. Able to upload a new event to Parse

--- app/src/main/java/com/codepath/tout_le_monde/CreateEventActivity.java
+++ app/src/main/java/com/codepath/tout_le_monde/CreateEventActivity.java
@@ -2,29 +2,49 @@ package com.codepath.tout_le_monde;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.Calendar;
 
 public class CreateEventActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    //Constants
+    private static final String TAG = "CreateEventActivity";
+
+    //Widgets
      private EditText etDescription;
-     private EditText etDate;
+//     private EditText etDate;
      private EditText etEventName;
-     private EditText etCampaign;
+//     private EditText etCampaign;
      private EditText etMaxParticipants;
      private TextView tvLocation;
+//     private EditText etStart;
+//     private EditText etEnd;
+//     private EditText etMeridiem;
      private Button btnSubmit;
-     private Spinner spinner;
+     private Spinner campaign_spinner;
+     private Spinner start_times_spinner;
+    private Spinner end_times_spinner;
+     private TextView mDisplayDate;
+     private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,20 +53,71 @@ public class CreateEventActivity extends AppCompatActivity implements AdapterVie
 
         etEventName = findViewById(R.id.etEventName);
         etDescription = findViewById(R.id.etDescription);
-        etDate = findViewById(R.id.etDate);
-        etCampaign = findViewById(R.id.etCampaign);
+//        etDate = findViewById(R.id.etDate);
+//        etCampaign = findViewById(R.id.etCampaign);
         etMaxParticipants = findViewById(R.id.etMaxParticipants);
         tvLocation = findViewById(R.id.tvLocation);
+//        etStart = findViewById(R.id.etStart);
+//        etEnd = findViewById(R.id.etEnd);
+//        etMeridiem = findViewById(R.id.etMeridiem);
         btnSubmit = findViewById(R.id.btnSubmit);
-        spinner = findViewById(R.id.spin);
+        campaign_spinner = findViewById(R.id.campaign_spinner);
+        start_times_spinner = findViewById(R.id.start_times_spinner);
+        end_times_spinner = findViewById(R.id.end_times_spinner);
+        mDisplayDate = findViewById(R.id.tvDate);
+
+        //Set up onclick listener for Date
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        CreateEventActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener, year, month, day);
+
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                String date = month + "/" + dayOfMonth + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
 
         //Set up Spinner
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.campaigns, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        campaign_spinner.setAdapter(adapter);
 //
 //        //Set listener for spinner
-        spinner.setOnItemSelectedListener(this);
+        campaign_spinner.setOnItemSelectedListener(this);
+
+
+        //Set up Spinner
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.times, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        start_times_spinner.setAdapter(adapter2);
+//
+//        //Set listener for spinner
+        start_times_spinner.setOnItemSelectedListener(this);
+
+
+        //Set up Spinner
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.times, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        end_times_spinner.setAdapter(adapter3);
+//
+//        //Set listener for spinner
+        end_times_spinner.setOnItemSelectedListener(this);
 
         //Grab location from google maps activity
         String location = getIntent().getStringExtra("Location");
@@ -56,37 +127,57 @@ public class CreateEventActivity extends AppCompatActivity implements AdapterVie
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Get data and convert to appropriate data type
+                String name = etEventName.getText().toString();
+                String campaign = campaign_spinner.getSelectedItem().toString();
+                String description = etDescription.getText().toString();
+                String location = tvLocation.getText().toString();
+                String start = start_times_spinner.getSelectedItem().toString();
+                String end = end_times_spinner.getSelectedItem().toString();
+//                String start = etStart.getText().toString();
+//                String end = etEnd.getText().toString();
+//                String meridiem = etMeridiem.getText().toString();
+                String max_participants = etMaxParticipants.getText().toString();
+                String date = mDisplayDate.getText().toString();
                 //Make event in Parse
-                //parseCreateEvent();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                saveEvent(name, campaign, date, location, max_participants, description, start, end, currentUser );
             }
         });
 
     }
 
-    private void parseCreateEvent() {
+    private void saveEvent(String name, String campaign, String date, String location, String max_participants, String description, String start, String end, ParseUser host) {
 
-        String event_name = etEventName.getText().toString();
-        String event_campaign = etCampaign.getText().toString();
-        String location = tvLocation.getText().toString();
-        String description = etDescription.getText().toString();
-        String max_participants = etMaxParticipants.getText().toString();
-        int max_people = 0;
+        Event event = new Event();
+        event.setName(name);
+        event.setCampaign(campaign);
+        event.setDate(date);
+        event.setDescription(description);
+        event.setLocation(location);
+        event.setStartTime(start);
+        event.setEndTime(end);
+        event.setMaxParticipants(max_participants);
+        event.setHost(host);
 
-        if (max_participants != "N/A") {
+        //save to parse
+        event.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    //This means we got an error
+                    Log.e(TAG, "Error while saving", e);
+                    Toast.makeText(CreateEventActivity.this, "Error while saving", Toast.LENGTH_SHORT).show();
+                }
 
-            max_people = Integer.parseInt(max_participants);
+                else {
+                    Log.i(TAG, "Event creation was successful");
+                    Toast.makeText(CreateEventActivity.this, "Event Created", Toast.LENGTH_SHORT).show();
 
-        }
+                }
+            }
+        });
 
-        ParseObject new_Event = new ParseObject("Event");
-        new_Event.put("Name", event_name);
-        new_Event.put("Campaign", event_campaign);
-        new_Event.put("Location", location);
-        new_Event.put("Description", description);
-        new_Event.put("Max_Participants", max_people);
-
-        //Save new event
-        new_Event.saveInBackground();
 
     }
 
--- app/src/main/java/com/codepath/tout_le_monde/Event.java
+++ app/src/main/java/com/codepath/tout_le_monde/Event.java
@@ -13,9 +13,34 @@ import java.util.Date;
 @ParseClassName("Event")
 public class Event extends ParseObject {
 
+    //Don't forget to add a date attribute once we can save an event to Parse
+    //look up more about the date object
+
+    public static final String KEY_NAME = "Name";
+    public static final String KEY_CAMPAIGN = "Campaign";
+    public static final String KEY_DATE = "Date";
     public static final String KEY_DESCRIPTION = "Description";
+    public static final String KEY_LOCATION = "Location";
+    public static final String KEY_MAX_PARTICIPANTS = "Max_Participants";
+    public static final String KEY_HOST = "Host";
+    public static final String KEY_START_TIME = "Start";
+//    public static final String KEY_MERIDIEM = "Meridiem";
+    public static final String KEY_END_TIME = "End";
     public static final String KEY_IMAGE = "Image";
-    public static final String KEY_USER = "user";
+
+
+    public String getName() {
+        return getString(KEY_NAME);
+    }
+
+    public void setName(String name) { put(KEY_NAME, name); }
+
+
+    public String getDate() {
+        return getString(KEY_DATE);
+    }
+
+    public void setDate(String date) { put(KEY_DATE, date); }
 
     public String getDescription() {
         return getString(KEY_DESCRIPTION);
@@ -33,11 +58,65 @@ public class Event extends ParseObject {
         put(KEY_IMAGE, parseFile);
     }
 
-    public ParseUser getUser() {
-        return getParseUser(KEY_USER);
+    public ParseUser getHost() {
+        return getParseUser(KEY_HOST);
     }
 
-    public void setUser(ParseUser user) {
-        put(KEY_USER, user);
+    public void setHost(ParseUser host) {
+        put(KEY_HOST, host);
     }
+
+    public String getCampaign() {
+        return getString(KEY_CAMPAIGN);
+    }
+
+    public void setCampaign(String campaign) {
+        put(KEY_CAMPAIGN, campaign);
+    }
+
+
+    public String getLocation() {
+        return getString(KEY_LOCATION);
+    }
+
+    public void setLocation(String location) {
+        put(KEY_LOCATION, location);
+    }
+
+
+    public String getMaxParticipants() {
+        return getString(KEY_MAX_PARTICIPANTS);
+    }
+
+    public void setMaxParticipants(String max_participants) {
+        put(KEY_MAX_PARTICIPANTS, max_participants);
+    }
+
+
+    public String getStartTime() {
+        return getString(KEY_START_TIME);
+    }
+
+    public void setStartTime(String time) {
+        put(KEY_START_TIME, time);
+    }
+
+
+    public String getEndTime() {
+        return getString(KEY_END_TIME);
+    }
+
+    public void setEndTime(String e_time) {
+        put(KEY_END_TIME, e_time);
+    }
+
+//
+//    public String getMeridiem() {
+//        return getString(KEY_MERIDIEM);
+//    }
+//
+//    public void setMeridiem(String meridiem) {
+//        put(KEY_MERIDIEM, meridiem);
+//    }
+
 }
