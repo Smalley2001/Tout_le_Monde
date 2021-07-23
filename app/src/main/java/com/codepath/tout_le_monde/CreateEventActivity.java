713af44c5982b2d5a4be1744d34592e2cb340164
Dorian Smalley <smalley2001@fb.com>
2021-07-23 17:18:20 -0400

    Upgraded CreateEventActivity to include form validation and changed UI

--- app/src/main/java/com/codepath/tout_le_monde/CreateEventActivity.java
+++ app/src/main/java/com/codepath/tout_le_monde/CreateEventActivity.java
@@ -18,8 +18,10 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
 import com.parse.ParseException;
-import com.parse.ParseObject;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
@@ -27,36 +29,73 @@ import java.util.Calendar;
 
 public class CreateEventActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    private static final String TAG = "CreateEventActivity";
+    private static final String TAG = "TestActivity";
 
-     private EditText EtDescription;
-     private EditText EtEventName;
-     private EditText EtMaxParticipants;
-     private TextView TvLocation;
-     private Button BtnSubmit;
-     private Spinner CampaignSpinner;
-     private Spinner StartTimesSpinner;
-    private Spinner EndTimesSpinner;
-     private TextView MDisplayDate;
-     private DatePickerDialog.OnDateSetListener MDateSetListener;
+    private Button btnSubmitEvent;
+    private EditText etName;
+    private TextView tvCampaign;
+    private TextView tvLocation;
+    private TextView tvDate;
+    private Spinner startSpin;
+    private Spinner endSpin;
+    private EditText etMax;
+    private EditText etDescription;
+
+    private AwesomeValidation awesomeValidation;
+    private DatePickerDialog.OnDateSetListener MDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
 
-        EtEventName = findViewById(R.id.etEventName);
-        EtDescription = findViewById(R.id.etDescription);
-        EtMaxParticipants = findViewById(R.id.etMaxParticipants);
-        TvLocation = findViewById(R.id.tvLocation);
-        BtnSubmit = findViewById(R.id.btnSubmit);
-        CampaignSpinner = findViewById(R.id.campaign_spinner);
-        StartTimesSpinner = findViewById(R.id.start_times_spinner);
-        EndTimesSpinner = findViewById(R.id.end_times_spinner);
-        MDisplayDate = findViewById(R.id.tvDate);
+        btnSubmitEvent = findViewById(R.id.btn_Submit_Event);
+        etName = findViewById(R.id.et_name);
+        tvCampaign = findViewById(R.id.tv_campaign);
+        tvLocation = findViewById(R.id.tv_location);
+        tvDate = findViewById(R.id.tv_date);
+        startSpin = findViewById(R.id.starting_spinner);
+        endSpin = findViewById(R.id.ending_spinner);
+        etMax = findViewById(R.id.et_max);
+        etDescription = findViewById(R.id.et_description);
 
-        //Set up onclick listener for Date
-        MDisplayDate.setOnClickListener(new View.OnClickListener() {
+        // Initialize Validation Style
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+        awesomeValidation.addValidation(this,R.id.et_name, RegexTemplate.NOT_EMPTY,
+                R.string.invalid_name);
+        awesomeValidation.addValidation(this,R.id.et_max, RegexTemplate.NOT_EMPTY,
+                R.string.invalid_max);
+
+        awesomeValidation.addValidation(this,R.id.et_description, RegexTemplate.NOT_EMPTY,
+                R.string.invalid_description);
+
+        btnSubmitEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (awesomeValidation.validate()) {
+                    // On Success
+                    // Make event in Parse
+                    String name = etName.getText().toString();
+                    String campaign = tvCampaign.getText().toString();
+                    String description = etDescription.getText().toString();
+                    String location = tvLocation.getText().toString();
+                    String start = startSpin.getSelectedItem().toString();
+                    String end = endSpin.getSelectedItem().toString();
+                    String max_participants = etMax.getText().toString();
+                    String date = tvDate.getText().toString();
+                    ParseUser currentUser = ParseUser.getCurrentUser();
+                    saveEvent(name, campaign, date, location, max_participants, description, start, end, currentUser);
+                    goEventTimeLineActivity();
+                    Toast.makeText(getApplicationContext(), "Event Created", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Event Creation Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // Set up onclick listener for Date
+        tvDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Calendar cal = Calendar.getInstance();
@@ -78,60 +117,56 @@ public class CreateEventActivity extends AppCompatActivity implements AdapterVie
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 month = month + 1;
                 String date = month + "/" + dayOfMonth + "/" + year;
-                MDisplayDate.setText(date);
+                tvDate.setText(date);
             }
         };
 
         // Set up Spinner
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.campaigns, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        CampaignSpinner.setAdapter(adapter);
-//
-//        // Set listener for spinner
-        CampaignSpinner.setOnItemSelectedListener(this);
-
-
-        // Set up Spinner
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.times, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        StartTimesSpinner.setAdapter(adapter2);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        startSpin.setAdapter(adapter2);
 //
 //        // Set listener for spinner
-        StartTimesSpinner.setOnItemSelectedListener(this);
+        startSpin.setOnItemSelectedListener(this);
 
 
         // Set up Spinner
         ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.times, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        EndTimesSpinner.setAdapter(adapter3);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        endSpin.setAdapter(adapter3);
 //
 //        // Set listener for spinner
-        EndTimesSpinner.setOnItemSelectedListener(this);
+        endSpin.setOnItemSelectedListener(this);
 
         // Grab location from google maps activity
-        String location = getIntent().getStringExtra("Location");
+        String location = getIntent().getStringExtra("MapsLocation");
+        String campaign = getIntent().getStringExtra("Campaign");
+        Log.i(TAG, "  Received campaign: " + campaign);
 
-        TvLocation.setText(location);
-
-        BtnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String name = EtEventName.getText().toString();
-                String campaign = CampaignSpinner.getSelectedItem().toString();
-                String description = EtDescription.getText().toString();
-                String location = TvLocation.getText().toString();
-                String start = StartTimesSpinner.getSelectedItem().toString();
-                String end = EndTimesSpinner.getSelectedItem().toString();
-                String max_participants = EtMaxParticipants.getText().toString();
-                String date = MDisplayDate.getText().toString();
-                // Make event in Parse
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                saveEvent(name, campaign, date, location, max_participants, description, start, end, currentUser );
-                goEventTimeLineActivity();
-            }
-        });
+        tvLocation.setText(location);
+        tvCampaign.setText(campaign);
 
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    private void goEventTimeLineActivity() {
+
+        Intent i = new Intent(CreateEventActivity.this, EventTimelineActivity.class);
+        startActivity(i);
+    }
+
 
     private void saveEvent(String name, String campaign, String date, String location, String max_participants, String description, String start, String end, ParseUser host) {
 
@@ -160,24 +195,6 @@ public class CreateEventActivity extends AppCompatActivity implements AdapterVie
                 }
             }
         });
-
-
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-
-    private void goEventTimeLineActivity() {
-
-        Intent i = new Intent(CreateEventActivity.this, EventTimelineActivity.class);
-        startActivity(i);
-    }
 }
