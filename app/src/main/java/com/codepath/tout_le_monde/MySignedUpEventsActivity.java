c4fe4254ed3bf3af087924539441110e38c4f061
Dorian Smalley <smalley2001@fb.com>
2021-07-27 13:08:11 -0400

    Implemented Signed Up Events activity so the user can see the events they signed up for, the details about those events, and allow them to cancel their sign up if necessary

---
+++ app/src/main/java/com/codepath/tout_le_monde/MySignedUpEventsActivity.java
@@ -0,0 +1,76 @@
+package com.codepath.tout_le_monde;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySignedUpEventsActivity extends EventTimelineActivity {
+
+    private RecyclerView rvMyEvents;
+    private EventsAdapter myadapter;
+    private List<Event> myallEvents;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_signed_up_events);
+
+        rvMyEvents = findViewById(R.id.rvMyEvents);
+
+        // initialize the array that will hold events and create a EventsAdapter
+        myallEvents = new ArrayList<>();
+        myadapter = new EventsAdapter(this, myallEvents);
+
+        // set the adapter on the recycler view
+        rvMyEvents.setAdapter(myadapter);
+        // set the layout manager on the recycler view
+        rvMyEvents.setLayoutManager(new LinearLayoutManager(this));
+        // query posts from EventTimeline
+        queryEvents();
+    }
+
+    private void queryEvents() {
+
+        // specify what type of data we want to query - Event.class
+        ParseQuery<Event> query = ParseQuery.getQuery(Event.class);
+        // include data referred by user key
+        query.include(Event.KEY_PARTICIPANTS);
+        query.whereEqualTo(Event.KEY_PARTICIPANTS, ParseUser.getCurrentUser());
+
+        query.setLimit(20);
+        // order posts by creation date (newest first)
+        query.addDescendingOrder("createdAt");
+
+        query.findInBackground(new FindCallback<Event>() {
+            @Override
+            public void done(List<Event> events, ParseException e) {
+                // check for errors
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+
+                for (Event event : events) {
+                    Log.i(TAG, "Apples: " + event.getDescription());
+                }
+
+                // save received events to list and notify adapter of new data
+                myallEvents.addAll(events);
+                myadapter.notifyDataSetChanged();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/codepath/tout_le_monde/SignedUpEventDetailsActivity.java
@@ -0,0 +1,162 @@
+package com.codepath.tout_le_monde;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.DeleteCallback;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+public class SignedUpEventDetailsActivity extends AppCompatActivity {
+
+    private Button btnCancelSignUp;
+    private Event event;
+    private TextView EventName;
+    private TextView EventHost;
+    private TextView EventDate;
+    private TextView EventLocation;
+    private TextView EventDescription;
+    private TextView EventCampaign;
+    private TextView EventStart;
+    private TextView EventEnd;
+    private TextView EventMax;
+    private TextView EventAvailable;
+    private static final String TAG = "SignedUpEventDetails";
+    private ParseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signed_up_event_details);
+
+
+        btnCancelSignUp = findViewById(R.id.signed_details_signup_cancel);
+        EventName = findViewById(R.id.signed_details_event_name);
+        EventHost = findViewById(R.id.signed_details_event_host);
+        EventLocation = findViewById(R.id.signed_details_event_location);
+        EventDescription = findViewById(R.id.signed_details_event_description);
+        EventCampaign = findViewById(R.id.signed_details_event_campaign);
+        EventStart = findViewById(R.id.signed_details_event_start);
+        EventEnd = findViewById(R.id.signed_details_event_end);
+        EventMax = findViewById(R.id.signed_details_event_max);
+        EventDate = findViewById(R.id.signed_details_event_date);
+        EventAvailable = findViewById(R.id.signed_details_event_available);
+
+        event = Parcels.unwrap(getIntent().getParcelableExtra("X"));
+        user = Parcels.unwrap(getIntent().getParcelableExtra("U"));
+        Log.d(TAG, String.format("Showing details for '%s'", event.getName()));
+        Log.d(TAG, "Username is: " + user.getUsername());
+
+        btnCancelSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser user = ParseUser.getCurrentUser();
+                ParseRelation<ParseUser> relation = event.getRelation("Participants");
+                relation.remove(user);
+                int count = event.getParticipantsCount();
+                count -= 1;
+                event.setParticipantsCount(count);
+                event.saveInBackground();
+                Toast.makeText(getApplicationContext(),"Canceled Sign Up", Toast.LENGTH_SHORT).show();
+                goMySignedUpEvents();
+            }
+        });
+
+        int maximum_participants = getMaxPeople();
+        String available_spots = calculateAvailableSpots(maximum_participants);
+        try {
+            bindFields(available_spots);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void goMySignedUpEvents() {
+        Intent i = new Intent(SignedUpEventDetailsActivity.this, MySignedUpEventsActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    private int getMaxPeople() {
+
+        String max = event.getMaxParticipants();
+        Log.i(TAG, "Max is: " + max);
+        int max_people = -1;
+        try {
+            max_people = Integer.parseInt(max);
+        } catch (NumberFormatException e) {
+            max_people = 0;
+            Log.i(TAG, "Could not convert!");
+        }
+        return max_people;
+    }
+
+    private String calculateAvailableSpots(int max_people) {
+
+        String available_spots = "";
+
+        if (max_people == 0) {
+            available_spots = "No Limit";
+        } else {
+
+            int users_signed_up = event.getParticipantsCount();
+            int available = max_people - users_signed_up;
+            available_spots = String.valueOf(available);
+
+        }
+
+        Log.i(TAG, "Available spots are: " + available_spots);
+        return available_spots;
+    }
+
+    private void bindFields(String available_spots) throws ParseException {
+
+        String name = "Event Name: " + event.getName();
+        String campaign = "Event Campaign: " + event.getCampaign();
+        String description = "Event Description: " + event.getDescription();
+        String max = "Apples";
+
+        if (event.getMaxParticipants() != null) {
+            max = "Maximum Participants: " + event.getMaxParticipants();
+            Log.i(TAG, "Maximum is: " + max);
+        } else {
+            max = "Maximum Participants: N/A";
+        }
+
+        Log.i(TAG, "The max is: " + max);
+
+        String date = "Event Date: " + event.getDate();
+        String location = "Event Location: " + event.getLocation();
+        String start = "Start Time: " + event.getStartTime();
+        String end = "End Time: " + event.getEndTime();
+        String host = "Event Host: " + event.getHostUsername();
+        String available = "Remaining spots: " + available_spots;
+
+
+        EventName.setText(name);
+        EventLocation.setText(location);
+        EventDescription.setText(description);
+        EventMax.setText(max);
+        EventDate.setText(date);
+        EventCampaign.setText(campaign);
+        EventStart.setText(start);
+        EventEnd.setText(end);
+        EventAvailable.setText(available);
+        EventHost.setText(host);
+    }
+}
